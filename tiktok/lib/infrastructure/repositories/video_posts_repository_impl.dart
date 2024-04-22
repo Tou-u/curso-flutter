dfd36be7fc570c0ddeb323bf5707cc2d6a3568a3
Tou-u <rodrignacio99@hotmail.com>
2024-04-22 08:49:22 -0400

    curso tiktok completo

---
+++ tiktok/lib/infrastructure/repositories/video_posts_repository_impl.dart
@@ -0,0 +1,19 @@
+import 'package:tiktok/domain/datasources/video_posts_datasource.dart';
+import 'package:tiktok/domain/entities/video_post.dart';
+import 'package:tiktok/domain/repositories/video_posts_repository.dart';
+
+class VideoPostsRepositoryImpl implements VideoPostRepository {
+  final VideoPostDatasource videosDatasource;
+
+  VideoPostsRepositoryImpl({required this.videosDatasource});
+
+  @override
+  Future<List<VideoPost>> getFavoriteVideosByUser(String userId) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<VideoPost>> getTrendingVideosByPage(int page) {
+    return videosDatasource.getTrendingVideosByPage(page);
+  }
+}
