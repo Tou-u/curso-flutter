0b1f722a223c1a26a3b5713e4a6d4ca74b20bf3b
Tou-u <rodrignacio99@hotmail.com>
2024-04-26 14:36:45 -0400

    Listado de actores

---
+++ cinemapedia/lib/presentation/providers/actors/actors_by_movie_provider.dart
@@ -0,0 +1,32 @@
+import 'package:cinemapedia/domain/entities/actor.dart';
+import 'package:cinemapedia/presentation/providers/actors/actors_repository_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final actorsByMovieProvider =
+    StateNotifierProvider<ActorsByMovieNotifier, Map<String, List<Actor>>>(
+  (ref) {
+    final fetchActors = ref.watch(actorsRepositoryProvider).getActorsByMovie;
+    return ActorsByMovieNotifier(getActors: fetchActors);
+  },
+);
+
+// {
+//   '505752': <Actor>[]
+// }
+
+typedef GetActorsCallback = Future<List<Actor>> Function(String movieId);
+
+class ActorsByMovieNotifier extends StateNotifier<Map<String, List<Actor>>> {
+  final GetActorsCallback getActors;
+
+  ActorsByMovieNotifier({
+    required this.getActors,
+  }) : super({});
+
+  Future<void> loadActors(String movieId) async {
+    if (state[movieId] != null) return;
+
+    final actors = await getActors(movieId);
+    state = {...state, movieId: actors};
+  }
+}
