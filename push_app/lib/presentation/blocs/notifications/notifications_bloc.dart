009b10a1cb6021a0f90fbfce2bf22fe3279da9e6
Tou-u <rodrignacio99@hotmail.com>
2024-04-29 14:13:46 -0400

    notificaciones locales

--- push_app/lib/presentation/blocs/notifications/notifications_bloc.dart
+++ push_app/lib/presentation/blocs/notifications/notifications_bloc.dart
@@ -1,8 +1,10 @@
+import 'dart:ffi';
 import 'dart:io';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:push_app/config/local_notifications/local_notifications.dart';
 
 import 'package:push_app/firebase_options.dart';
 import 'package:push_app/domain/entities/push_message.dart';
@@ -19,7 +21,18 @@ Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
 class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
   FirebaseMessaging messaging = FirebaseMessaging.instance;
 
-  NotificationsBloc() : super(const NotificationsState()) {
+  final Future<void> Function()? requestLocalNotificationPermissions;
+  final void Function({
+    required int id,
+    String? title,
+    String? body,
+    String? data,
+  })? showLocalNotification;
+
+  NotificationsBloc({
+    this.requestLocalNotificationPermissions,
+    this.showLocalNotification,
+  }) : super(const NotificationsState()) {
     on<NotificationStatusChanged>(_notificationStatusChanged);
     on<NotificationReceived>(_onPushMessageReceived);
 
@@ -84,6 +97,15 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
           : message.notification!.apple?.imageUrl,
     );
 
+    if (showLocalNotification != null) {
+      showLocalNotification!(
+        id: 1,
+        title: notification.title,
+        body: notification.body,
+        data: notification.messageId,
+      );
+    }
+
     add(NotificationReceived(notification));
   }
 
@@ -101,6 +123,12 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
       provisional: false,
       sound: true,
     );
+
+    // Solicitar permiso a las local notifications
+    if (requestLocalNotificationPermissions != null) {
+      await requestLocalNotificationPermissions!();
+      // await LocalNotifications.requestPermissionLocalNotifications();
+    }
 
     add(NotificationStatusChanged(settings.authorizationStatus));
   }
